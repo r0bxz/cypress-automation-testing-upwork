af0eafbcb921cfbb8b8483483b22dd0ad2238b36
ma7mood-r0b <m.alrob1@student.aaup.edu>
2024-08-27 10:46:47 +0300

    Corporate Branding

--- cypress/e2e/Admin/corporateBrandingPage.cy.js
+++ cypress/e2e/Admin/corporateBrandingPage.cy.js
@@ -1,39 +1,43 @@
-import CorporateBrandingPage from '../../pages/Admin/corporateBrandingPage';
+import corporateBrandingPage from '../../pages/Admin/corporateBrandingPage';
 import loginPage from '../../pages/Login/loginPage';
 import loginData from '../../fixtures/loginData.json';
 
-describe('Corporate Branding tests', () => {
-    
-  
-    before(() => {
-      cy.visit('http://localhost/orangehrm-5.7/orangehrm-5.7');
+describe('Corporate Branding Page Tests', () => {
+  before(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    loginPage.enterUsername(loginData.validUser.username);
+    loginPage.enterPassword(loginData.validUser.password);
+    loginPage.clickSignIn();
+  });
+
+  beforeEach(() => {
+    cy.session('login', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
       loginPage.enterUsername(loginData.validUser.username);
       loginPage.enterPassword(loginData.validUser.password);
       loginPage.clickSignIn();
     });
-  
-    beforeEach(() => {
-      cy.session('login', () => {
-        cy.visit('http://localhost/orangehrm-5.7/orangehrm-5.7/');
-        loginPage.enterUsername(loginData.validUser.username);
-        loginPage.enterPassword(loginData.validUser.password);
-        loginPage.clickSignIn();
-      });
-      cy.visit('http://localhost/orangehrm-5.7/orangehrm-5.7/web/index.php/admin/addTheme');
-    });
-  it('should select colors and upload files', () => {
-    // Select colors
-    CorporateBrandingPage.selectPrimaryColor('#FF5733');
-    CorporateBrandingPage.selectSecondaryColor('#33FF57');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/admin/addTheme');
+  });
 
-    // Upload files
-    CorporateBrandingPage.uploadClientLogo('logo.png');
-    CorporateBrandingPage.uploadClientBanner('banner.png');
-    CorporateBrandingPage.uploadLoginBanner('login-banner.png');
+  it('should change primary and secondary colors', () => {
+    corporateBrandingPage.selectColor(0, '#FF5733'); 
+    corporateBrandingPage.selectColor(1, '#33FF57'); 
+    corporateBrandingPage.clickPreview();
+    corporateBrandingPage.clickPublish();
+  });
 
-    // Interact with buttons
-    CorporateBrandingPage.clickResetToDefault();
-    CorporateBrandingPage.clickPreview();
-    CorporateBrandingPage.clickPublish();
+  it('should upload client logo and banners', () => {
+    corporateBrandingPage.uploadFile(0, 'logo.png'); 
+    corporateBrandingPage.uploadFile(1, 'banner.png'); 
+    corporateBrandingPage.uploadFile(2, 'loginBanner.png'); 
+    corporateBrandingPage.clickPreview();
+    corporateBrandingPage.clickPublish();
+  });
+
+  it('should reset branding to default settings', () => {
+    corporateBrandingPage.clickResetToDefault();
+    corporateBrandingPage.clickPreview();
+    corporateBrandingPage.clickPublish();
   });
 });
--- cypress/pages/Admin/corporateBrandingPage.js
+++ cypress/pages/Admin/corporateBrandingPage.js
@@ -1,45 +1,29 @@
-class CorporateBrandingPage {
-    selectPrimaryColor(color) {
-      cy.get('div[data-v-ae9dff95] .oxd-color-input').eq(0).click();
-      this.setColor(color);
+class corporateBrandingPage {
+
+    selectColor(colorPickerIndex, colorHex) {
+      cy.get('.oxd-color-input').eq(colorPickerIndex).click(); 
+      cy.get('.oxd-color-picker-label').contains('HEX').next('input').clear().type(colorHex); 
     }
   
-    selectSecondaryColor(color) {
-      cy.get('div[data-v-ae9dff95] .oxd-color-input').eq(1).click();
-      this.setColor(color);
+    uploadFile(fileInputIndex, fileName) {
+      cy.get('input[type="file"]').eq(fileInputIndex).attachFile(fileName);
     }
   
-  
-    setColor(color) {
-      cy.get('input.oxd-color-picker-range').invoke('val', color).trigger('change');
-      cy.get('input.oxd-input').clear().type(color);
-      cy.get('div[data-v-2d1384ea] .oxd-color-picker-indicator').click(); 
-    }
-  
-    uploadClientLogo(filePath) {
-      cy.get('.oxd-file-button').eq(0).attachFile(filePath);
-    }
-  
-    uploadClientBanner(filePath) {
-      cy.get('.oxd-file-button').eq(1).attachFile(filePath);
-    }
-  
-    uploadLoginBanner(filePath) {
-      cy.get('oxd-file-button').eq(2).attachFile(filePath);
-    }
-  
+    
     clickResetToDefault() {
       cy.contains('button', 'Reset to Default').click();
     }
   
+    
     clickPreview() {
       cy.contains('button', 'Preview').click();
     }
   
+    
     clickPublish() {
       cy.contains('button', 'Publish').click();
     }
   }
   
-  export default new CorporateBrandingPage();
+  export default new corporateBrandingPage();
   
